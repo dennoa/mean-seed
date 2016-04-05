d0ebb2cc518811ff2104e80eaa0b6e893f9bccd7
denno <andrew.dennison@live.com.au>
2016-04-05 13:31:34 +1000

    add state generator function for OAuth

--- client/services/auth/auth.js
+++ client/services/auth/auth.js
@@ -4,14 +4,25 @@
  * Sets up satellizer for OAuth
  */
 angular.module('meanSeed').config(function($authProvider) {
+
+  function stateToAvoidCsrf() {
+    var rand = Math.random().toString(36).substr(2);
+    return encodeURIComponent(rand);
+  }
+
   $authProvider.google({
-    clientId: 'GOOGLE_CLIENT_ID'
+    clientId: 'GOOGLE_CLIENT_ID',
+    optionalUrlParams: ['display', 'state'],
+    state: stateToAvoidCsrf
   });
   $authProvider.github({
-    clientId: 'GITHUB_CLIENT_ID'
+    clientId: 'GITHUB_CLIENT_ID',
+    optionalUrlParams: ['scope', 'state'],
+    state: stateToAvoidCsrf
   });
   $authProvider.linkedin({
-    clientId: 'LINKEDIN_CLIENT_ID'
+    clientId: 'LINKEDIN_CLIENT_ID',
+    state: stateToAvoidCsrf
   });
 
 }).run(function($http, toUrl, SatellizerConfig) {
